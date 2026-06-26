b3a804b8e11b5e1b1fe0e5d275c919ea9b0590cf
ggreen <gregoryg@vmware.com>
2026-06-26 18:18:09 -0400

    using Rabbit streams

--- deployments/local/start-rabbitmq.sh
+++ deployments/local/start-rabbitmq.sh
@@ -1,8 +1,22 @@
-podman run --rm -it \
-  --name rabbitmq \
+PLUGINS="rabbitmq_jms,rabbitmq_jms_management,rabbitmq_amqp1_0,rabbitmq_shovel,rabbitmq_shovel_management,rabbitmq_stream,rabbitmq_stream_browser,rabbitmq_stream_management,rabbitmq_delayed_queue"
+
+echo "Starting Tanzu RabbitMQ with initial plugins: $PLUGINS..."
+
+podman run -it --rm \
+  --name tanzu-rabbitmq \
   -p 5672:5672 \
-  -p 15672:15672 \
   -p 5552:5552 \
-  -e RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS="-rabbitmq_stream advertised_host \"localhost\"" \
-  -e RABBITMQ_PLUGINS_WITH_TO_START="rabbitmq_stream rabbitmq_management" \
-  rabbitmq:4.3-management
+  -p 15672:15672 \
+  -p 1883:1883 \
+  -e RABBITMQ_ENABLED_PLUGINS="$PLUGINS" \
+  -e RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS="-rabbitmq_stream advertised_host localhost -rabbitmq_stream advertised_port 5552" \
+  rabbitmq.packages.broadcom.com/vmware-tanzu-rabbitmq:4.3.0
+
+#podman run --rm -it \
+#  --name rabbitmq \
+#  -p 5672:5672 \
+#  -p 15672:15672 \
+#  -p 5552:5552 \
+#  -e RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS="-rabbitmq_stream advertised_host \"localhost\"" \
+#  -e RABBITMQ_PLUGINS_WITH_TO_START="rabbitmq_stream rabbitmq_management" \
+#  rabbitmq:4.3-management
