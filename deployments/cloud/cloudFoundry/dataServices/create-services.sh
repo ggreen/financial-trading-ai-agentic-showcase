7cec8168c9bb0347415998fd84716b8b7ceddf7a
ggreen <gregoryg@vmware.com>
2026-08-26 15:03:04 -0400

    cleanup

--- deployments/cloud/cloudFoundry/dataServices/create-services.sh
+++
@@ -1,7 +0,0 @@
-cf create-service p.rabbitmq on-demand-plan rabbitmq -c '{"plugins": {"rabbitmq_stream": true}}'
-
-
-cf create-service p.redis on-demand-cache  valkey
-
-cf create-service postgres on-demand-postgres-db  postgres
-cf create-service p-cloudcache extra-small gemfire
